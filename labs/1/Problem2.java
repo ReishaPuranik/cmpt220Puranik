361fb8b81cde94a4ce84f4db4095a98374fb5d1b
ReishaPuranik <54602814+ReishaPuranik@users.noreply.github.com>
2020-02-07 12:18:16 -0500

    Problem 2

---
+++ labs/1/Problem2.java
@@ -0,0 +1,12 @@
+
+public class Problem2 {
+
+	public static void main(String[] args) {
+		double formula1 = 4*(1.-(1./3)+(1./5)-(1./7)+(1./9)-(1./11));
+		System.out.println(formula1);
+		double formula2 = 4*(1.-(1./3)+(1./5)-(1./7)+(1./9)-(1./11)+(1./13));
+		System.out.println(formula2);
+
+	}
+
+}
