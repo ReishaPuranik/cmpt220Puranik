03e24f00406b2e46b6b1fa76c53aafc49a938009
ReishaPuranik <54602814+ReishaPuranik@users.noreply.github.com>
2020-02-28 16:57:34 -0500

    People

---
+++ prj/1/People.java
@@ -0,0 +1,12 @@
+public class People {
+
+	String Name;
+
+	StringBuilder Class;
+
+	public People(String theName, StringBuilder theClass) {
+		//Initializing
+		Name = theName;
+		Class = theClass;
+	}
+}
