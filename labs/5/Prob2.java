b255a1b5b2042e711f4abfef485b46033f6624b3
ReishaPuranik <54602814+ReishaPuranik@users.noreply.github.com>
2020-03-05 19:00:22 -0500

    Problem 2

---
+++ labs/5/Prob2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Prob2 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		double array[] = new double[10];
+		System.out.println("Enter 10 numbers: ");
+		for (int i = 0; i < array.length; i++) {
+			array[i] = input.nextInt();
+		}
+		System.out.println(mean(array));
+		System.out.println(deviation(array));
+	}
+	public static double deviation(double[] x) {
+		double sum2 = 0;
+		double sum = 0;
+		for (int i = 0; i < x.length; i++) {
+			sum = x[i] + sum;
+		}
+		sum = sum/10;
+		for (int i = 0; i < x.length; i++) {
+			sum2 = Math.pow(x[i] - sum, 2) + sum2;
+		}
+		sum2 = sum2/10;
+		return Math.sqrt(sum2);		
+	}
+	public static double mean(double[] x) {
+		double sum = 0;
+		for (int i = 0; i < x.length; i++) {
+			sum = x[i] + sum;
+		}
+		sum = sum/10;
+		return sum;
+	}
+}
