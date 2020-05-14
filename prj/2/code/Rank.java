3d1f6cc1335345f372f933bd3fd4bcd8c48fe911
ReishaPuranik <54602814+ReishaPuranik@users.noreply.github.com>
2020-05-14 13:13:43 -0400

    Rank Class

---
+++ prj/2/code/Rank.java
@@ -0,0 +1,7 @@
+//creating an abstract class for the rank
+abstract public class Rank {
+	private int getAverage;
+	public int totalAvg;
+	//used in each class to calculate the average
+	abstract public double getAverage();
+}
